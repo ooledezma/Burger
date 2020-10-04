4f86f3faefb6e9d48502a90a2567c981913963dc
Oscar Ledezma <ooledezma@gmail.com>
2020-10-03 20:21:51 -0700

    Updated to jawsdb url

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,32 @@
 // Connectiong configuration
 require("dotenv").config();
 
-
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+//Set up JAWS DB
+var connection;
 
-  // Your username
-  user: process.env.DB_USERNAME,
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
 
-  // Your password
-  password: process.env.DB_PASSWORD,
+    // Your username
+    user: process.env.DB_USERNAME,
 
-  //your database
-  database: process.env.DB_DATABASE,
-});
+    // Your password
+    password: process.env.DB_PASSWORD,
+
+    //your database
+    database: process.env.DB_DATABASE,
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
